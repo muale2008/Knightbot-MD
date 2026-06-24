bba8ae8d9291fad485c6113f4966af0c2ebf53b7
muale2008 <nanagyeman79@gmail.com>
2026-06-24 22:08:44 +0000

    Update bot settings for Evel Bot

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Evel Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Evel Bot",
+  botOwner: 'Evel', // Your name
+  ownerNumber: '233206777968', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
